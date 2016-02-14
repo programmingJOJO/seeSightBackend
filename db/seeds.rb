b97a123c913254af57f2161e48b896b03ffb1d11
Jojo <johannes.koelbl@web.de>
2016-02-14 21:48:10 +0100

    Make first test mit frontend app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Place.create(name: 'Test1', description: 'Ein Beispieltext', place_type: 0, city_id: 1)
+Place.create(name: 'Test2', description: 'Noch ein Beispieltext', place_type: 0, city_id: 1)
+Place.create(name: 'Test3', description: 'Und noch ein Beispieltext', place_type: 0, city_id: 1)
